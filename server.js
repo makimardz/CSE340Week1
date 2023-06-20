d298b8b7ddeeed33249a983fc183157569272e64
makimardz <makimardz@gmail.com>
2023-06-20 23:06:03 +0800

    with error handlers

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities")
 
 /* ***********************
  * View Engine and Template
@@ -25,12 +26,30 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(require("./routes"))
 app.get("/", baseController.buildHome);
-// Inventory routes
-app.use("/inv", inventoryRoute);
 
 // Index route
 app.get("/", function(req, res) {
   res.render("index", { title: "Home" })
+})
+// Inventory routes
+app.use("/inv", inventoryRoute);
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
 })
 
 /* ***********************
@@ -46,3 +65,5 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+
