cb52e36d14fbefec8a7477ba6afb64febb55624a
makimardz <makimardz@gmail.com>
2023-06-27 12:26:52 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ router.use(express.static("public"));
 router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
-
+router.use("/favicon.ico", express.static(__dirname + "public/images/favicon.ico"));
+router.get("/favicon.ico", (req, res) => res.status(204));
 
 module.exports = router;
 
